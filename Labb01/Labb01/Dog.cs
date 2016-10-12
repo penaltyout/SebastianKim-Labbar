dd23fa75b1a70a5bdbe2989b853e2909c2f01355
Sebastian Kim <penaltyout@gmail.com>
2016-10-12 14:03:09 +0200

    My first commit

---
+++ Labb01/Labb01/Dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb01
+{
+    class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Breed { get; set; }
+
+        public Dog()
+        {
+        }
+
+        public Dog(string name, int age, string breed)
+        {
+            Name = name;
+            Age = age;
+            Breed = breed;
+        }
+
+        public string Introduction()
+        {
+            return String.Format("My dog's name is {0}, and he is {1} years old. He is a {2}.", Name, Age, Breed);
+        }
+    }
+}
