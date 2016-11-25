18e9a51086d5b5b0c2e3c5a58740f85163c2ee66
Sebastian Kim <kimseongchin@gmail.com>
2016-11-25 14:08:53 +0100

    Practice debugging

---
+++ Debugging01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            double x = 1;
+
+            Console.Write("1 / x : ? ");
+            line = Console.ReadLine();
+
+            try
+            {
+                x = Convert.ToDouble(line);
+                x = 1 / x;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            Console.WriteLine("1 / x = " + x.ToString());
+            Console.ReadKey();
+        }
+    }
+}
